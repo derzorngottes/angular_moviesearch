1efee75b34fa932baa0450fc55916696edf662de
derzorngottes <amy.conchie@gmail.com>
2016-05-16 19:12:24 -0700

    Refactor controller

--- app/movies/Movie.controller.js
+++ app/movies/Movie.controller.js
@@ -10,11 +10,10 @@
   function Movie($location, $routeParams, MovieService) {
     var vm = this;
 
-    MovieService.getMovie($route.current.params.id).then(function(movie) {
+    MovieService.getMovie($routeParams.id).then(function(movie) {
       vm.movie = movie.data;
     })
     .catch(err).then(function(err) {
-      console.log(err);
       vm.error = err;
     });
   }
